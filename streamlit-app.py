95197ef5c0e4275f1692512897e5f726c64d3b76
cheongqinxue <58881356+cheongqinxue@users.noreply.github.com>
2022-08-03 11:14:33 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -9,20 +9,26 @@ st.set_page_config(layout="wide")
 
 @st.cache
 def load(f1, f2):
-    
     with FS.open(f1) as f:
         cluster_data = json.loads(f.read())
-            
     with FS.open(f2) as f:
         df = pd.read_json(f, lines=True)
+    
+    v2count = []
+    for i,c in cluster_data:
+        v2count.append({
+            'cluster':i,
+            'count':len(c['members'])
+        })
+        
     df_ = df.dropna(subset='similar_group_id')
     sims = df_[df_.similar_group_id.str.isnumeric()].similar_group_id.unique()
     count = df_.similar_group_id.value_counts().to_dict()
     sims = [s for s in sims if count[s]>1]
-    return cluster_data, df, sims
+    return cluster_data, df, sims, df_.similar_group_id.value_counts(), pd.DataFrame(v2count)
 
 def main():
-    cluster_data, df, sims = load(st.secrets['CLUSTER'], st.secrets['ALL'])
+    cluster_data, df, sims, v1_counts, v2_counts = load(st.secrets['CLUSTER'], st.secrets['ALL'])
     with st.sidebar:
         display = st.selectbox(
         'Display mode',
@@ -39,11 +45,14 @@ def main():
             cluster = None
 
     if display=='V2 clusters':
+        st.subheader('Displaying members and neighbors of clusters detected by Version 2')
+        st.barchart(v2_counts.sort_values(by='count').head(20)
         st.caption('Cluster members')
         st.table(pd.DataFrame(cluster_data[cluster]['members'])[['title','content']])
         st.caption('Cluster neighbours')
         st.table(pd.DataFrame(cluster_data[cluster]['neighbours'])[['title','content']])
     else:
+        st.subheader('Displaying members of clusters detected by Version 1')
         st.table(df[df.similar_group_id==sim].head(300))
 
 if __name__=='__main__':
